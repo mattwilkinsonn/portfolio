1be2107338783f23d2fe542f030c7b6bad2ac65b
Matt Wilkinson <mattwilki17@gmail.com>
2021-02-09 12:41:12 -0500

    first commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-templates-post-js": hot(preferDefault(require("/home/mattw/git/portfolio/src/templates/post.js"))),
+  "component---src-templates-tag-js": hot(preferDefault(require("/home/mattw/git/portfolio/src/templates/tag.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/mattw/git/portfolio/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/mattw/git/portfolio/src/pages/404.js"))),
+  "component---src-pages-archive-js": hot(preferDefault(require("/home/mattw/git/portfolio/src/pages/archive.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/mattw/git/portfolio/src/pages/index.js"))),
+  "component---src-pages-pensieve-index-js": hot(preferDefault(require("/home/mattw/git/portfolio/src/pages/pensieve/index.js"))),
+  "component---src-pages-pensieve-tags-js": hot(preferDefault(require("/home/mattw/git/portfolio/src/pages/pensieve/tags.js")))
+}
+
